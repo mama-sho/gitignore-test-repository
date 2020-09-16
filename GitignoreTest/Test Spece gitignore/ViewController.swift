3bce775de27be6e918b3415a81471f866a7e08a2
上田大樹 <uedadaiki@MacBook-Pro.local>
2020-09-16 15:44:51 +0900

    gitignoreスペース反応

---
+++ GitignoreTest/Test Spece gitignore/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  GitignoreTest
+//
+//  Created by 上田大樹 on 2020/09/16.
+//  Copyright © 2020 ueda.daiki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
